c10ec37838b8f81fce658c54bcf45e42566aac00
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-11-11 09:31:06 -0300

    Initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,28 @@
+import Fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
+
+async function bootstrap() {
+    const fastify = Fastify({
+        logger: true,
+    })
+
+    await fastify.register(cors, {
+        origin: true,
+    })
+
+
+    fastify.get('/login', async () => {
+        const users = await prisma.user
+        
+        return { users }
+    })
+
+    await fastify.listen({ port: 3333 })
+}
+
+bootstrap()
