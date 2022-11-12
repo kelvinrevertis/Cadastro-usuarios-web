36089ceedcc0dd2538fb03e86b17721153b0e84f
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-11-12 11:48:39 -0300

    Create login page

---
+++ web/src/pages/Login.tsx
@@ -0,0 +1,11 @@
+export function Login() {
+    return (
+        <>
+            <h1>Olá visitante!</h1>
+            <input type="text" placeholder="E-mail, CPF ou PIS" />
+            <input type="text" placeholder="senha" />
+            <button>Login</button>
+            <button>Cadastrar</button>
+        </>
+    )
+}
