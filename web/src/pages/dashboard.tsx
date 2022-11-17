287c20f91a5e6237011ce623168d188b54c89ca6
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-11-17 20:57:12 -0300

    Logout button

--- web/src/pages/dashboard.tsx
+++ web/src/pages/dashboard.tsx
@@ -1,24 +1,25 @@
-import { useContext, useEffect } from "react"
-import { AuthContext } from "../contexts/AuthContext"
 import { api } from "../hooks/axios"
-import { parseCookies } from 'nookies'
+import { parseCookies, destroyCookie } from 'nookies'
 import { useRouter } from 'next/router'
 import Link from "next/link"
 
+export function logout(ctx) {
+    alert('Usuario deslogado!')
+    destroyCookie(ctx, 'register.token')
+    console.log('teste')
+}
 
 function Dashboard({ userName }) {
     const arrayName = userName.split(" ")
     const firstName = arrayName[0]
 
-
-    const route = useRouter();
     return (
-
 
         <div>
 
             <h1>Olá {firstName} </h1>
             <Link href="/update" >Editar perfil</Link>
+            <Link href="/signin" onClick={logout} >Logout</Link>
 
         </div>
 
@@ -29,6 +30,7 @@ function Dashboard({ userName }) {
 export default Dashboard
 
 export const getServerSideProps = async (ctx) => {
+
     const { ['register.token']: token } = parseCookies(ctx)
 
     if (!token) {
@@ -39,6 +41,7 @@ export const getServerSideProps = async (ctx) => {
             }
         }
     }
+
     const { data: user } = await api.get('/login', { headers: { authorization: `Bearer ${token}` } })
 
     const userName = user.user.name
