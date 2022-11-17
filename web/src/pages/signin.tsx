862eb37f86966eed542a2291db341b2314905da2
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-11-17 14:19:57 -0300

    SigIn page

--- web/src/pages/signin.tsx
+++ web/src/pages/signin.tsx
@@ -2,41 +2,61 @@ import { useContext, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { AuthContext } from "../contexts/AuthContext";
+import { useForm } from 'react-hook-form'
+import { parseCookies } from "nookies";
 
 export default function SignIn() {
+    const { register, handleSubmit } = useForm();
     const router = useRouter()
-    const auth = useContext(AuthContext)
+    const { signIn } = useContext(AuthContext)
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
 
-    async function handleLogin() {
-        console.log("teste")
-        if (email && password) {
-            console.log("teste2")
-            try {
-                router.push('/dashboard')
-            } catch (error) {
-                console.log(error)
-            }
-        }
+
+
+    async function handleSignIn(data) {
+        await signIn(data)
     }
     return (
 
         <div>
             <h1>Olá visitante!</h1>
+            <form onSubmit={handleSubmit(handleSignIn)}>
 
-            <input type="text" value={email} placeholder="E-mail, CPF ou PIS"
-                onChange={event => setEmail(event.target.value)} />
-            <input type="text" value={password} placeholder="senha"
-                onChange={event => setPassword(event.target.value)} />
-            <button onClick={handleLogin} >SignIn</button>
+                <input {...register('email')} type="text"
+                    value={email} placeholder="E-mail, CPF ou PIS"
+                    onChange={event => setEmail(event.target.value)} />
+
+                <input {...register('password')}
+                    type="text" value={password} placeholder="senha"
+                    onChange={event => setPassword(event.target.value)} />
+
+                <button>SignIn</button>
+            </form>
             <Link href="/registeruser" >Cadastrar</Link>
 
 
         </div>
     )
 
+}
+
+export const getServerSideProps = async (ctx) => {
+    const token = parseCookies(ctx)
+
+    if (token["register.token"]) {
+        return {
+            redirect: {
+                destination: '/dashboard',
+                permanent: false,
+            }
+        }
+    }
+
+    return {
+        props: {}
+    }
 }
 
